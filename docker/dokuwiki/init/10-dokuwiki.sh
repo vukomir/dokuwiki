fc46636d69be7ead3615c4df7e3dbc06e59517a3
Vucomir Ianculov <vucomir.ianculov@azets.com>
2018-04-07 04:25:52 -0700

    updated dokuwiki startup

--- docker/dokuwiki/init/10-dokuwiki.sh
+++ docker/dokuwiki/init/10-dokuwiki.sh
@@ -26,33 +26,7 @@ if [ ! -f /var/www/html/lib/tpl/index.php ]; then
 fi
 
 if [ ! -f /var/www/html/lib/plugins/index.html ]; then
-  echo "Init lib/tpl folder"
+  echo "Init lib/plugins folder"
   cp -R /opt/src/dokuwiki/lib/plugins/* /var/www/html/lib/plugins/
   chown -R www-data /var/www/html/lib/plugins
 fi
-
-
-# Lib upgrade
-function copy_extra_lib {
-  FOLDER_ORIGIN=$1
-  FOLDER_DESTINATION=$2
-  for FILEPATH_ORIGIN in ${FOLDER_ORIGIN}/*; do
-    if [ -d "${FILEPATH_ORIGIN}" ]; then
-      FILENAME=$(basename "${FILEPATH_ORIGIN}")
-      if [ ! -d "${FOLDER_DESTINATION}/${FILENAME}" ]; then
-        echo "Keeping Lib: ${FILENAME}";
-        cp -R ${FILEPATH_ORIGIN} ${FOLDER_DESTINATION}/${FILENAME}
-      fi
-    fi
-  done
-}
-
-rm -fr /opt/tmp/lib
-mkdir -p /opt/tmp/lib
-cp -R /opt/src/dokuwiki/lib/* /opt/tmp/lib
-
-copy_extra_lib "/var/www/html/lib/plugins" "/opt/tmp/lib/plugins"
-copy_extra_lib "/var/www/html/lib/tpl" "/opt/tmp/lib/tpl"
-rm -fr /var/www/html/lib/*
-cp -R /opt/tmp/lib/* /var/www/html/lib/
-chown -R www-data /var/www/html/lib
